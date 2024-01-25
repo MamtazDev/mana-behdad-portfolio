0d52adbb8a28fad1bb823b8da478fa783d31cbfc
tonni-akter-dev <tanniakhter123@gmail.com>
2024-01-25 11:49:08 +0600

    animation added

--- src/components/Work/CaseStudy/CaseStudy.jsx
+++ src/components/Work/CaseStudy/CaseStudy.jsx
@@ -3,21 +3,45 @@ import case_study1 from '../../../assets/case-study.png'
 import case_study2 from '../../../assets/case-study2.png'
 import arrow from '../../../assets/arrow.png'
 import { Link } from 'react-router-dom'
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const CaseStudy = () => {
+
+    Aos.init();
 
     return (
         <>
             <div className="bg-black pt-[120px] flex justify-center items-center flex-col text-white">
                 <div className='mb-[71px]'>
-                    <img className='mb-[41px]' src={case_study1} alt="" />
-                    <Link to="/firstCaseStudy">
+                    <img data-aos="fade-bottom"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true" className='mb-[41px]' src={case_study1} alt="" />
+                    <Link to="/firstCaseStudy" data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="2000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true">
                         <button className='bg-[#131313]  rounded-[10px] py-[20px] text-[36px] font-semibold font-sans flex items-center gap-[30px] w-[450px] justify-center'>View Case study  <img src={arrow} alt="" /></button>
                     </Link>
                 </div>
                 <div className='mb-[120px]'>
-                    <img className='mb-[41px]' src={case_study2} alt="" />
-                    <Link to="/secondCaseStudy">
+                    <img data-aos="fade-bottom"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true" className='mb-[41px]' src={case_study2} alt="" />
+                    <Link to="/secondCaseStudy" data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="2000"
+                        data-aos-easing="ease-in-out"
+                        data-aos-mirror="true">
                         <button className='bg-[#131313] rounded-[10px] py-[20px] text-[36px] font-semibold font-sans flex items-center gap-[30px] w-[450px] justify-center'>View Case study  <img src={arrow} alt="" /></button>
                     </Link>
                 </div>
--- src/components/Work/WorkBanner/WorkBanner.jsx
+++ src/components/Work/WorkBanner/WorkBanner.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useTypingEffect } from '../../../hooks/typing-effect';
 
 const WorkBanner = () => {
+
+  AOS.init();
+  const text = useTypingEffect("Mana", 500);
+
   return (
-    <div className="work_banner_bg text-white ">
+    <div className="text-white work_banner_bg ">
       <div className='max-w-screen-xl mx-auto pt-[189px]'>
-        <h1 className='font-normal font-outfit text-[68px] text-white mb-[36px] '>
-          Hey there, I am Mana <br />
-          a <span className='text-[#8BEAAD]'> UX/UI</span>
-          designer based <br /> in San Francisco, CA
-        </h1>
-        <p className='text-[32px] font-outfit font-normal leading-normal '>Architect turned UX/UI enthusiast. I design digital <br /> spaces where form meets function, weaving <br /> aesthetics and user experience seamlessly. Join me in <br />  creating visually stunning and intuitively crafted <br /> interfaces that leave a lasting impression. Welcome  <br /> to my design journey!</p>
-      </div>
-    </div>
+        <div className='font-normal font-outfit text-[68px] text-white mb-[36px]  '
+          data-aos="fade-right"
+          data-aos-offset="200"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true">
+          Hey there, I am    {text} <br />
+
+          <span className='mb-0'>a <span className='text-[#8BEAAD]'> UX/UI</span>
+            designer based</span>
+
+          <br /> in San Francisco, CA
+        </div>
+        <p className='text-[32px] font-outfit font-normal leading-normal' data-aos="fade-left"
+          data-aos-offset="200"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true" >
+          Architect turned UX/UI enthusiast. I design digita
+          <br />  spaces where form meets function, weaving
+          <br />
+          aesthetics and user experience seamlessly. Join me in
+          <br />
+          creating visually stunning and intuitively crafted
+          <br />
+          interfaces that leave a lasting impression.Welcome
+          <br />
+          to my design journey!
+        </p >
+      </div >
+    </div >
   )
 }
 
